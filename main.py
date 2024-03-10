a70a841974437b489d93f6cd06a564171b747cac
Alekya Arra <alekyarra07@gmail.com>
2024-03-10 12:36:37 +0530

    added new endpoint

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app= FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get('/index/', response_class=HTMLResponse)
+@app.get('/', response_class=HTMLResponse)
 def index(request: Request):
     context={'request': request}
     return templates.TemplateResponse("index.html", context)
+
+@app.get('/index', response_class=HTMLResponse)
+def index(request: Request):
+    context={'request': request}
+    return templates.TemplateResponse("index1.html", context)
